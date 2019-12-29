e114dac2dcc2d203bcd3b0091e73b2088ba38e84
Matt Mukerjee <mukerjee@nefeli.io>
2019-12-29 06:42:28 +0000

    ease https reverse proxying

--- run_autorebase.sh
+++ run_autorebase.sh
@@ -4,8 +4,12 @@ docker run -d \
        -p 127.0.0.1:3000:3000 \
        -e APP_ID \
        -e WEBHOOK_SECRET \
-       -e PRIVATE_KEY_PATH \
+       -e PRIVATE_KEY \
        -e DEBUG=* \
+       -e VIRTUAL_HOST \
+       -e VIRTUAL_PORT \
+       -e LETSENCRYPT_HOST \
+       -e LETSENCRYPT_EMAIL \
        -v /etc/timezone:/etc/timezone:ro \
        --name autorebase \
        nefelinetworks/autorebase
